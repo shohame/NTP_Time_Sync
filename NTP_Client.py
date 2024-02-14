26d4e9ead3a5e935f5f67e5f658b7227c2fe06b9
855718 <perry.shoham@gmail.com>
2024-02-14 21:22:58 +0200

    update

--- NTP_Client.py
+++ NTP_Client.py
@@ -1,6 +1,7 @@
 import socket
 
 import subprocess
+import time
 from datetime import datetime
 
 
@@ -22,5 +23,23 @@ def time_client(server_host='localhost', server_port=12345):
         # This step is platform dependent and requires admin rights
         set_system_time_windows(server_time)
 
+def test_time_diff(server_host='localhost', server_port=12345):
+
+    while(True):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((server_host, server_port))
+            server_time = s.recv(1024).decode('utf-8')
+            sec_diff = (datetime.now() - datetime.strptime(server_time, '%Y-%m-%d %H:%M:%S')).seconds
+            msec_diff = sec_diff * 1000 + (datetime.now() - datetime.strptime(server_time, '%Y-%m-%d %H:%M:%S')).microseconds / 1000
+
+            print(f"Server time is: {server_time} and the difference is {msec_diff} ms.")
+
+        time.sleep(1)
+
 if __name__ == "__main__":
-    time_client()
+
+    SERV_IP = "10.0.0.3"
+    SERV_PORT = 6000
+
+    time_client(server_host=SERV_IP , server_port=SERV_PORT)
+    test_time_diff(server_host=SERV_IP , server_port=SERV_PORT)
