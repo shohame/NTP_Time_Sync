0c8d6cf8f9e3272b842d769ea5906fec0a5264f9
Perry Shoham <Perry.Shoham@gmail.com>
2024-02-26 17:24:13 +0200

    Added .bat file to run on windows, and .sh files for Linux.

---
+++ linux_run_client.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Navigate to the directory where the script is located
+cd "$(dirname "$0")"
+
+# Execute the Python module
+python3 -m NTP_Client
+
---
+++ linux_run_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Navigate to the directory where the script is located
+cd "$(dirname "$0")"
+
+# Execute the Python module
+sudo python3 -m NTP_Server
+
